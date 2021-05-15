079c0aee49a3ee0eb1a2759edf3a8fd7fc900169
msm17b019 <sujeetkumariiit@gmail.com>
2021-05-15 10:07:12 +0530

    commit bst in python.

---
+++ BST.py
@@ -0,0 +1,65 @@
+class BinarySearchTree:
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+
+    def add_child(self,data):
+        if data==self.data:
+            return
+
+        if data < self.data:
+            if self.left:
+                self.left.add_child=data
+            else:
+                self.left=BinarySearchTree(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right=BinarySearchTree(data)
+
+    def in_order_traversal(self):
+        elements=[]
+
+        if self.left:
+            elements+=self.left.in_order_traversal()
+
+        elements.append(self.data)
+
+        if self.right:
+            elements+=self.right.in_order_traversal()
+
+        return elements
+
+    def search(self,value):
+        if self.data==value:
+            return True
+
+        if value<self.data:
+            if self.left:
+                return self.left.search(value)
+            else:
+                return False
+
+        if value>self.data:
+            if self.right:
+                return self.right.search(value)
+            else:
+                return False
+
+            
+
+def build_bst(elements):
+    root=BinarySearchTree(elements[0])
+
+    for i in range(1,len(elements)):
+        root.add_child(elements[i])
+
+    return root
+
+if __name__=='__main__':
+    numbers=[17,4,1,20,9,23,18,34]
+    numbers_tree=build_bst(numbers)
+    # print(numbers_tree.in_order_traversal())
+    print(numbers_tree.search(18))
