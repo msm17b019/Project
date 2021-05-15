96ce08347b64d9db1d8d62e30c5ad69a364aff52
msm17b019 <sujeetkumariiit@gmail.com>
2021-05-15 11:53:28 +0530

    commit bst part 1

--- BST_1.py
+++ BST_1.py
@@ -10,7 +10,7 @@ class BinarySearchTree:
 
         if data < self.data:
             if self.left:
-                self.left.add_child=data
+                self.left.add_child(data)
             else:
                 self.left=BinarySearchTree(data)
         else:
@@ -32,6 +32,30 @@ class BinarySearchTree:
 
         return elements
 
+    def pre_order_traversal(self):
+        elements=[self.data]
+
+        if self.left:
+            elements+=self.left.pre_order_traversal()
+
+        if self.right:
+            elements+=self.right.pre_order_traversal()
+
+        return elements
+
+    def post_order_traversal(self):
+        elements=[]
+
+        if self.left:
+            elements+=self.left.post_order_traversal()
+
+        if self.right:
+            elements+=self.right.post_order_traversal()
+
+        elements.append(self.data)
+
+        return elements
+
     def search(self,value):
         if self.data==value:
             return True
@@ -48,6 +72,24 @@ class BinarySearchTree:
             else:
                 return False
 
+    def find_min(self):
+        if self.left is None:
+            return self.data
+        
+        return self.left.find_min()
+
+    def find_max(self):
+        if self.right is None:
+            return self.data
+        
+        return self.right.find_max()
+
+    def calculate_sum(self):
+        left_sum=self.left.calculate_sum() if self.left else 0 
+        right_sum=self.right.calculate_sum() if self.right else 0
+        return self.data+left_sum+right_sum
+
+
             
 
 def build_bst(elements):
@@ -62,4 +104,9 @@ if __name__=='__main__':
     numbers=[17,4,1,20,9,23,18,34]
     numbers_tree=build_bst(numbers)
     # print(numbers_tree.in_order_traversal())
-    print(numbers_tree.search(18))
+    # print(numbers_tree.search(18))
+    # print(numbers_tree.find_min())
+    # print(numbers_tree.find_max())
+    # print(numbers_tree.calculate_sum())
+    # print(numbers_tree.pre_order_traversal())
+    # print(numbers_tree.post_order_traversal())
